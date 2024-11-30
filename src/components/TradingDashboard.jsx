ec47de8e66e2202e2e17917307f26acad2ca8c0f
Mich120232024 <132930015+Mich120232024@users.noreply.github.com>
2024-11-30 17:47:51 +0100

    Add TradingDashboard component

---
+++ src/components/TradingDashboard.jsx
@@ -0,0 +1,3 @@
+import React, { useState } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, AreaChart, Area } from 'recharts';
+import { CheckCircleIcon, ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
